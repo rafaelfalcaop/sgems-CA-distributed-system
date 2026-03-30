c9dcdd1c77750c233eedc3fc4db869ae078c71c6
rafaelfalcaop <rafael.falcao@outlook.com>
2026-03-30 12:06:32 +0100

    Initial Maven project setup with package structure and proto files

---
+++ src/main/java/org/sgems/sgems/ca/distributed/system/SgemsCADistributedSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.sgems.sgems.ca.distributed.system;
+
+/**
+ *
+ * @author rafaelfalcao
+ */
+public class SgemsCADistributedSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
